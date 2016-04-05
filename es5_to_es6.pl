664792ff5eeb711fe972238178c4fa6c6b2e103e
Eirik Birkeland <birketrans@gmail.com>
2016-04-06 00:13:02 +0200

    strict/warnings

--- es5_to_es6.pl
+++ es5_to_es6.pl
@@ -1,18 +1,19 @@
 # WARNING: This will affect any example code in comments as well!
 # WARNING 2: This requires a properly formatted file
 #test
+use strict;
+use warnings;
 
 foreach (<>) {
    # Change anonymous functions style
-   s/function\s*(\(.*?\))\s*\{/$1=>{/g;
+   s/function\s*(\(.*?\))\s*\{/$1 => {/g;
    # Remove parenthesis around single argument in anonymous functions
-   s/\(([a-zA-Z\$-_]+)\)=>\{/$1=>{/g;
+   s/\(([a-zA-Z\$-_]+)\)=>\{/$1 => {/g;
    # change var to let
    s/^\bvar\b/let/;
    # Remove unnecessary semicolons ;)
    s/;$//;
-   # Add semicolons where actually needed!
+   # Add semicolons where actually needed (in front of parentheses and brackets)
    s/^(?=[\(\{])/;/g;
-   
    print;
 }
