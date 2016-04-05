152ebd6a2ecbf2a60bb8c94a1431064e1d3d080d
Eirik Birkeland <birketrans@gmail.com>
2016-04-06 00:27:10 +0200

    .

--- es5_to_es6.pl
+++ es5_to_es6.pl
@@ -5,15 +5,23 @@ use strict;
 use warnings;
 
 foreach (<>) {
+   # Skip if the line is a standard comment (/* */ not implemented)
+   next if m{^\s*(?://};
+
    # Change anonymous functions style
    s/function\s*(\(.*?\))\s*\{/$1 => {/g;
+
    # Remove parenthesis around single argument in anonymous functions
    s/\(([a-zA-Z\$-_]+)\)=>\{/$1 => {/g;
+
    # change var to let
-   s/^\bvar\b/let/;
+   s/^(\s*)\bvar\b/$1let/;
+
    # Remove unnecessary semicolons ;)
-   s/;$//;
+   s/;(\s*)$/$1/;
+
    # Add semicolons where actually needed (in front of parentheses and brackets)
    s/^(?=[\(\{])/;/g;
+
    print;
 }
