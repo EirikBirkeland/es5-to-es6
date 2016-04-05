9c15ac58a2e93b19c6afbab057c0d0912a42b598
Eirik Birkeland <birketrans@gmail.com>
2016-04-06 01:02:14 +0200

    .

--- es5_to_es6.pl
+++ es5_to_es6.pl
@@ -5,8 +5,8 @@ use strict;
 use warnings;
 
 foreach (<>) {
-   # Skip if the line is a standard comment (/* */ not implemented)
-   next if m{^\s*(?://};
+   # Skip line if the line is a standard comment (/* */ not implemented)
+   next if m{^\s*(?://)};
 
    # Change anonymous functions style
    s/function\s*(\(.*?\))\s*\{/$1 => {/g;
@@ -14,8 +14,14 @@ foreach (<>) {
    # Remove parenthesis around single argument in anonymous functions
    s/\(([a-zA-Z\$-_]+)\)=>\{/$1 => {/g;
 
+   # Add spacing around any arrows lacking it
+   s/\s*=>\s*(?=\{)/ => /g;
+
    # change var to let
    s/^(\s*)\bvar\b/$1let/;
+
+   # change require lines from let to const
+   s/^(\s*)\blet/$1const/ if m{=\s*require\(.*?\)};
 
    # Remove unnecessary semicolons ;)
    s/;(\s*)$/$1/;
